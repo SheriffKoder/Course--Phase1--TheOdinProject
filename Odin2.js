96be8d174af9b5e56bfa9498b3497a258e7f14b8
SheriffKoder <sheriff.koder@hotmail.com>
2022-09-09 23:49:34 +0200

    Notes about HTML-Lists

--- Odin2.js
+++ Odin2.js
@@ -1,3 +1,33 @@
+/*
+getElementById();
+getElementsByClassName('x'); 
+//all elements with this class name and then becomes an array of elements
+getElementsByTagName('x'); //array
+.querySelector();   //returns the first element, any selector tag/class/id
+querySelectorAll();
+
+ const btn = document.getElementById('btn')
+ btn.addEventListener('click', function x() {}  );
+//     btn.classList.add('button')
+
+
+[DOM Manoeuvering]
+.lastElementChild //its last child
+.children[3] //its child #3
+.parentNode
+.nextElementSibling
+.previousElementSibling
+
+document.insertBefore(newNode, existingNode)
+parentEl.insertBefore(createEl, firstchildEl)
+parentEl.replaceChild(createEl, firstchildEl)
+parentEl.removeChild(firstchildEl)
+
+btn.classList.toggle('button_blue'); //toggle the class on and off
+
+
+*/
+
 
 let btn = document.getElementById('btn');
 btn.addEventListener('click', addClassFunction);
@@ -5,3 +35,12 @@ btn.addEventListener('click', addClassFunction);
 function addClassFunction () {
     btn.classList.toggle('button_blue');
 }
+
+
+/* List Switcher */
+
+const container = document.querySelector(".container");
+container.addEventListener("change", (event) => {
+  const list = document.querySelector("ol");
+  list.setAttribute("style", `list-style-type: ${event.target.value}`);
+});
