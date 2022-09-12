bae0233b822c2fa5169ea64309f702c61f6faf26
SheriffKoder <sheriff.koder@hotmail.com>
2022-09-12 15:15:34 +0200

    Notes about Event switcher, li marker image, li horizontal list, float fix

--- Odin2.js
+++ Odin2.js
@@ -3,11 +3,11 @@ getElementById();
 getElementsByClassName('x'); 
 //all elements with this class name and then becomes an array of elements
 getElementsByTagName('x'); //array
-.querySelector();   //returns the first element, any selector tag/class/id
+.querySelector();   //returns the 'first element', any selector tag/class/id
 querySelectorAll();
 
- const btn = document.getElementById('btn')
- btn.addEventListener('click', function x() {}  );
+const btn = document.getElementById('btn');
+btn.addEventListener('click', function x() {}  );
 //     btn.classList.add('button')
 
 
@@ -37,10 +37,26 @@ function addClassFunction () {
 }
 
 
-/* List Switcher */
+/* List Switcher -----------------------------------------------------*/
+/*take the input-container, add to it an event listener on change, 
+this event will take the list and set to it a style of 
+event's target's value which is input-container */
 
 const container = document.querySelector(".container");
-container.addEventListener("change", (event) => {
+container.addEventListener("change", (event) => 
+{
   const list = document.querySelector("ol");
   list.setAttribute("style", `list-style-type: ${event.target.value}`);
-});
+}
+
+                            );
+
+/* --------------------------------------------------------------------*/
+
+
+/* textNode is the text for h1,p etc without using innerHTML */
+const h1 = document.createElement('h1');
+const TextNode = document.createTextNode('Hello World');
+h1.appendChild(TextNode);
+document.body.appendChild(h1);
+
