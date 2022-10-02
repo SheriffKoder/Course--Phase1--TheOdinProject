cb853d1c79b3c53b63dd994e5f8f9f06bf53548e
SheriffKoder <sheriff.koder@hotmail.com>
2022-10-02 16:06:19 +0200

    Notes: naming variables

--- Odin2.js
+++ Odin2.js
@@ -60,3 +60,77 @@ container.addEventListener("change", (event) =>
 //h1.appendChild(TextNode);
 //document.body.appendChild(h1);
 
+
+/*////////////////////////////////////////////////////////////////////*/
+
+/* //// javascript, adding new info ////
+
+variables are storage containers
+var, let, const                 // var was used in old browsers, and almost the same as let will discuss later
+
+let user = 'john', age = 25;    //can add multiple variables in one line 
+
+let user = 'john',              //multiline break
+    age = 25;
+
+//*--------------------*//*
+name should be letters, digits, $, _ 
+and not start with a digit, not use -
+let $, let _ are correct
+
+variable Apple and APPLE and apple
+are all different variables, case matters
+
+non-latin letters are allowed but not recommended
+
+
+//*--------------------*//*
+constant (unchanging), cannot be reassigned otherwise will cause error
+const 
+
+uppercase constants writing method, const COLOR_RED = "";
+for facts hardcoded in code and easy to remember/use than their values
+value is known prior to execution and directly written into the code.
+
+be descriptive, human readable good names, naming is an important skill
+
+
+//*--------------------*//*
+declared with var, are either function-scoped or global-scoped. 
+variables skip the block they are in (function or loop)
+
+function sayHi () {
+
+  if (true) {
+  var test = true; // use "var" instead of "let"
+  }
+
+  alert(test); //outputs true , but if let will error not defined
+
+  for (var i = 0; i < 10; i++) {
+  }
+  alert(i); //outputs i
+
+}
+
+sayHi();
+alert(test); //outputs undefined either ways
+
+*/
+
+function sayHi () {
+
+  if (true) {
+  var test = true; // use "var" instead of "let"
+  }
+
+  alert(test); //outputs true , but if let will error not defined
+
+  for (var i = 0; i < 10; i++) {
+  }
+  alert(i); //outputs i
+
+}
+
+sayHi();
+alert(test); //outputs undefined either ways
