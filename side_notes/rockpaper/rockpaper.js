e3368d707ffdb0a4955eae39b60f7ed5eb1420b0
SheriffKoder <sheriff.koder@hotmail.com>
2022-12-04 07:02:20 +0200

    Odin JS Notes: Git Branching

--- side_notes/rockpaper/rockpaper.js
+++ side_notes/rockpaper/rockpaper.js
@@ -7,7 +7,7 @@ let gameValues = {
     userScore: 0,
     ties: 0,
 
-}
+};
 
 
 
@@ -15,7 +15,7 @@ let gameValues = {
 function getComputerChoice () {
     
     let randomChoice = Math.floor(Math.random() * 3);
-    console.log('computer choose '+ gameValues['choicesOptions'][randomChoice]);
+    //console.log('computer choose '+ gameValues['choicesOptions'][randomChoice]);
 
     return gameValues['choicesOptions'][randomChoice];
 
@@ -47,36 +47,67 @@ function computerWinner (userInput,computerInput) {
         'scissors': {'rock': -1, 'paper':1, 'scissors':0 }
     };
 
-    console.log( 'your score is ', scoreObject[userInput][computerInput]);
+    //console.log( 'your score is ', scoreObject[userInput][computerInput]);
     return scoreObject[userInput][computerInput];
 
 }
 
 
+/*////////////////////////////////////////////////////////////////////*/
+/*////////////////////////////////////////////////////////////////////*/
 
 
 
 
-function game() {
+function game(e) {
 
     //play 5 rounds of playRound, keeps the score
     //reports a winner or loser at end
 
-    for (i=0; i<2; i++) {
+    //for (i=0; i<2; i++) {
 
-        let user = getUserInput();
+        let user = e.target.textContent.toLowerCase();
+        //let user = getUserInput();
         let computer = getComputerChoice();
         let score = computerWinner(user, computer);
         gameValues['userScore'] += score;
 
-    }
+        //console.log(user);
+        //console.log(computer);
+        //console.log("this round's score" + score);
+        //console.log("total score" + gameValues['userScore']);
+
+        document.getElementById("user_selection_display").textContent = "User Selected: " + user;
+        document.getElementById("computer_selection_display").textContent = "Computer Selected: " + computer;
+        document.getElementById("result_display").textContent = "Round Score: " + score;
+        document.getElementById("total_display").textContent = "Total Score: " + gameValues['userScore'];
+
+
+    //}
 
     //game score, if > 0 , user wins, if < 0 computer wins
-    console.log(gameValues['userScore']);
+    //console.log(gameValues['userScore']);
 
 
 
 
 }
 
-game();
+//game();
+
+let buttons = document.getElementsByTagName("button");
+
+for (button of buttons) {
+    button.addEventListener("click", game);
+
+}
+//playRound();
+
+function playRound (e) {
+
+    let selection = e.target.textContent.toLowerCase();
+    console.log(selection); 
+    return selection;
+
+
+}
