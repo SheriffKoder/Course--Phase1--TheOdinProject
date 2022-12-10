c8b3d6a7bd50991c6490048ec42bb08375edae1a
SheriffKoder <sheriff.koder@hotmail.com>
2022-12-10 10:46:52 +0200

    Odin JS Ex: 11 getTheTitles

---
+++ odin-js-exe/11_getTheTitles/getTheTitles.js
@@ -0,0 +1,41 @@
+const getTheTitle = function(input) {
+
+    console.log(input[0].title);
+    let output = [];
+
+    for (i=0; i<(input.length); i++) {
+        output.push(input[i].title);
+    }
+
+    console.log(output);
+
+};
+
+const getTheTitles = (input, output=[]) => {
+
+    for (i in input) {
+        output.push(input[i].title);
+    }
+    return output;
+
+};
+
+/*
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ];
+
+console.log(getTheTitles(books));
+*/
+
+
+// Do not edit below this line
+
+module.exports = getTheTitles;
