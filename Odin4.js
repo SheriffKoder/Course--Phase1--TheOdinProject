ca75b13d58bf87a586bf51aaee8a9dc64da51edc
SheriffKoder <sheriff.koder@hotmail.com>
2023-01-17 21:56:31 +0200

    JS Notes: Object constructors, Prototypes and Prototypal Inheritance

--- Odin4.js
+++ Odin4.js
@@ -43,6 +43,10 @@ const itemsCopy = [...items];
 or convert an object to an array
 const objectname = [...foo]; //but not use when adding methods inline to it
 
+
+
+
+
 /*////////////////////////////////////////////////////////////////////*/
 /*////////////////////////////////////////////////////////////////////*/
 /*
@@ -67,3 +71,70 @@ automatically format the code according to a set of rules
 
 
 */
+
+
+/*////////////////////////////////////////////////////////////////////*/
+/*////////////////////////////////////////////////////////////////////*/
+/*////////////////////////////////////////////////////////////////////*/
+/*////////////////////////////////////////////////////////////////////*/
+/*
+Object Constructors
+*/
+
+
+function Person(name2, marker2) {
+    this.name = name2;
+    this.marker = marker2;
+    this.sayName = function () {
+        //console.log(this.name);
+        return this.name;
+    }
+  }
+
+const player_one = new Person("steve", "X");
+//console.log(player_one.name); 
+//player_one.sayName();
+
+console.log(player_one.sayName()); //better practice 
+
+
+
+/*
+The Prototype
+all objects in js have a prototype, 
+which is another object that the original object inherits from
+the original prototype has access to all of its prototype's methods and properties
+
+prototypes are shared
+properties are copied with each new student
+
+*/
+
+Person.prototype.sayNameTwoTimes = function() {
+    console.log(this.name + " " + this.name);
+}
+
+player_one.sayNameTwoTimes();
+
+
+
+
+
+
+
+/* 
+Prototypal Inheritance 
+constructior prototype, create object of another object's prototype
+to use on the new constructor another's prototype which uses the same properties
+*/
+function Employee (name2) {
+    this.name = name2;
+}
+
+Employee.prototype = Object.create(Person.prototype);
+
+const mark = new Employee("mark");
+
+mark.sayNameTwoTimes();
+
+console.log(mark.constructor);
