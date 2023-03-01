bf7c5fbfb5b78a2eeb00364a0248f7958f67e73f
SheriffKoder <sheriff.koder@hotmail.com>
2023-03-01 23:32:37 +0200

    JS Notes: React introduction cont..

--- Odin5.js
+++ Odin5.js
@@ -131,6 +131,9 @@ class App extends Component {
 
 }
 
+//to access props in the constructor
+//call the parent class constructor by super(props)
+
 
 //combine javascript and JSX
 // render() return JSX (html like syntax) 
@@ -271,7 +274,8 @@ class Hello extends React.Component {
     constructor(props) {
     super(props);
     this.state = {
-      name: props.name
+      name: props.name // Not recommended, but OK if it's just used as seed data.
+            //this.props.name;?
     };
   }
 
@@ -297,10 +301,164 @@ not trigger a re-render of the component:
 this.state.comment = 'Hello'; // Don't do this
 this.setState({comment: 'Hello'}); // Do this
 
+//if state depends on a previous one, take into account the prev one
+//setState is asynchronous
+this.setState(function(prevState, prevProps) {
+  return {
+    counter: prevState.counter + prevProps.increment
+  };
+});
+
+
+//If we need to set a state with nested objects, we should spread all 
+//the levels of nesting in that object:
+
+this.setState(prevState => ({
+    ...prevState,
+    someProperty: {
+        ...prevState.someProperty,
+        someOtherProperty: {
+            ...prevState.someProperty.someOtherProperty, 
+            anotherProperty: {
+               ...prevState.someProperty.someOtherProperty.anotherProperty,
+               flag: false
+            }
+        }
+    }
+}))
+
+This can become cumbersome, so the use of the [immutability-helper](https://github.com/kolodny/immutability-helper) package is recommended.
+https://stackoverflow.com/questions/43040721/how-to-update-nested-state-properties-in-react
+
+check for more spreading nesting examples
+https://dev.to/colocodes/react-class-components-vs-function-components-23m6
+
+
+Life cycle methods:
+render()
+componentDidMount()
+componentDidUpdate()
+componentWillUnmount()
+shouldComponentUpdate()
+static getDerivedStateFromProps()
+getSnapshotBeforeUpdate()
+
+
+////ex////rendering function components is as same as class components
+const Welcome = (props) => {
+    const [timeOfDay, setTimeOfDay] = useState('morning');
+    return <h1>Hello, {props.name}, good {timeOfDay}</h1>;
+}
+
+//Render
+const element = <Welcome name="Sara" />;
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
+//////////
 
 
 
+////ex////
+//state in function components, 
+//define an array that will have to main elements
+//value of state and function to update said state
+//assign the useState hook(hook into react features) to that array
+//hooks are used when have a function component
+  and want to add some state to it
 
+
+import React, { useState } from 'react';
+
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+//but in class components we can use
+<button onClick={() => this.setState({ count: this.state.count + 1 })}>
+  Click me
+</button>
+
+
+//using props in function components
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  );
+}
+
+//choosing between class/function components depends on the project
+//but function components are popular now
+
+
+*/
+
+
+/*////////////////////////////////////////////////////////////////////*/
+/*////////////////////////////////////////////////////////////////////*/
+/*
+create-react-app
+tool Facebook developed which setups a complete react application for you
+setup, configuration 
+
+
+//a popular toolchain for building simple React Apps
+//toolchain: a set of distinct software development tools that are 
+    linked (or chained) together by specific stages
+In React development, different toolchains satisfy different requirements 
+for product development. For instance, Next.js is great for building a server-rendered website, 
+and GatsbyJS is optimized for static, content-oriented websites like blogs and newsletters.
+
+
+//to check this command
+npx create-react-app my-first-react-app
+cd my-first-react-app
+npm start   //to view the project in browser
+
+
+this will create index.js and App.js
+index.js: entry point for your application by default
+
+
+//render the app components into the DOM's id=root element
+//to change name, change "App" in .js and in index render also
+
+
+//folders
+//node modules: packages you want to use by calling an import statement
+//public: contains index.html and manifest.json
+
+//public/index.html: the main file served on the browser
+and bundled scripts (webpack bundle.js result) placed into the body
+webpack is used here
+html-webpack-plugin
+This final page is the one you get in build/index.html after 
+running npm run build, and the one that gets served from / when you 
+run npm start.
+
+//public/manifest.json
+describes your application
+used to populate the web app’s icons, colors, names, etc.
+and used by phones for example if a shortcut is added to home screen
+https://developers.google.com/web/fundamentals/engage-and-retain/web-app-manifest/
+
+
+//package.json
+contains scripts
 
 
 
