5c73a7d511803bbb53c46f4f946d101b2eb1d228
SheriffKoder <sheriff.koder@hotmail.com>
2023-03-02 23:56:01 +0200

    JS Notes: react introduction cont...

--- Odin5.js
+++ Odin5.js
@@ -910,6 +910,117 @@ render(<App/>, window..Document.getElementByID("app"));
 >>create webpack.config.js
 
 
+////// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+//__dirname (current directory), ./dist (directoy for bundle file) and its name
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+
+}
+
+/////////////////////
+
+>> create ./.babelrc
+
+{
+    "presets": ["env", "react"]
+}
+
+
+
+
+/////////////////////
+//package.json
+
+>> scrips > replace test
+with 
+"start" : webpack-dev-server --mode development --open --hot",
+"build" : "webpack --mode production"
+
+
+
+>> create src/index.html, index.js
+//this is the place for anything related to react application
+
+//import and write code normally
+
+//npm start
+
+
+//this is a file to use react without un-necessary stuff
+//with hot reload
+
+ctrl c to stop
+npm run build //will create the bundle file in dist folder
+
+https://www.youtube.com/watch?v=deyxI-6C2u4
+
+
+
+/*////////////////////////////////////////////////////////////////////*/
+/*////////////////////////////////////////////////////////////////////*/
+/*
+
+npm is a library of tools, libraries that you can download and use 
+
+in html
+<script src="https://unpkg.com/react016.5.1/....js"/>
+<script src="https://unpkg.com/react-dom016.5.1/....js"/>
+<script src="https://unpkg.com/babel/....js"/>
+
+
+in console write
+ReactDOM //this will give all ReactDOM methods
+
+const root = document.getElementById('root');
+
+const Element = React.createElement("div", [
+    className: 'container,
+    children:[
+        'Current Time', //text
+        Date.now().toLocaleString(),
+        React.createElement('span', [children: 'Hey there!']);
+    ] 
+])
+
+//ReactDOM.render(element, container, callback);
+ReactDOM.render(Element, root);
+
+//if got syntax error for using jsx
+//can enter babeljs.io
+//paste jsx code and will output a js code
+
+const string = `
+    sweet! ${js_expression}
+`;
+
+
+
+
+
 
 
 
