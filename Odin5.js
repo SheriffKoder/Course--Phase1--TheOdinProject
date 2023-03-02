287aee97d5b2a11f4bb3912705a325ad22adef03
SheriffKoder <sheriff.koder@hotmail.com>
2023-03-02 17:01:31 +0200

    JS Notes: react introduction cont...

--- Odin5.js
+++ Odin5.js
@@ -458,8 +458,456 @@ https://developers.google.com/web/fundamentals/engage-and-retain/web-app-manifes
 
 
 //package.json
-contains scripts
+contains scripts like start, build, test, eject
 
+Yarn is a package manager for JS dependencies
+alternative to the default Node.js package manager (npm)
+package managers allow use of packages/modules
+
+package.lock.json instead of yarn.lock if npm used when yarn not installed
+
+
+/*////////////////////////////////////////////////////////////////////*/
+/*
+
+Any application can be built by stripping stuff out and tweaking 
+the App.js
+
+can build React components separately inside a 
+src/components/folder and import these components inside App.js.
+
+There is a root component, <App>
+that imports other child components
+which in turn import other child components.
+
+Data flows from root to children through 
+React properties (called “props)
+and flows back up using callback functions.
+
+App.css file can be used for styling the application.
+
+
+step1: edit the card.js in src/components
+step2: edit App.js
+
+
+Yarn was created by Facebook in order to solve problems with npm
+Yarn has less installation duration, more secure
+
+
+/*////////////////////////////////////////////////////////////////////*/
+/*
+
+React Developer Tools
+extension to the browser's developer tools
+allows inspect react components that build the page
+
+helps check and edit component tree as well as props, state, hooks
+
+
+Debugging a react app
+can be when using chrome dev on a site using react
+the react button will be lighted up and
+there will be components tab after elements,sources etc
+
+
+
+Sometimes you may see components in the tree named Anonymous. 
+This is usually caused by developers exporting the component 
+as an unnamed default using syntax
+//you can change that by adding names after default
+export default () => {...};
+export default function() {...}
+export default class extends Component {...}
+
+
+////DOM element.render////
+//display a heading saying “Hello, world!” on the page.
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello, world!</h1>);
+
+
+React doesn't require using JSX
+but it allows react to show more useful error warning messages
+
+
+//declare name and use it inside jsx
+const name = "john";
+const element = <h1> Hello, {name} </h1>
+
+//you can put any javascript code inside curly brackets
+//for example 2+2, user.firstName, formatName(user) etc.
+
+
+
+//it is preferred to separate with line breaks and wrap in ();
+
+
+const element = (
+    <h1> 
+    Hello, {name} 
+    </h1> 
+);
+
+
+//after compilation, JSX expressions become regular JS function calls
+//and evaluate to JS objects
+
+// jsx lines can be used as variables
+// use inside if/for, argument, return it from functions
+
+
+// You should either use quotes (for string values) or curly braces 
+// (for expressions), but not both in the same attribute.
+const element = <img src={user.avatarUrl}></img>;
+const element = <a href="https://www.reactjs.org"> link </a>;
+
+
+**ReactDomElement = render.element = jsx html line = contains {js code}
+
+
+// as React uses camelCase property naming
+// for example class becomes className in JSX, and tabindex becomes tabIndex.
+
+// If a tag is empty, you may close it immediately with />, like XML:
+const element = <img src={user.avatarUrl} />;
+
+
+//React DOM ensured you never inject anything not explicity written
+in your application, everything is converted to a string before being rendered
+// this helps prevent XSS cross-site-scripting attacks
+
+
+
+//////////////////////////////////////
+//rendering an element
+//say there is a div with id root in html file
+
+//create it as a root
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+
+//create JSX element
+const element = <h1>Hello, world</h1>;
+
+//render the root with this element
+root.render(element);
+
+//example: ticking clock
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  root.render(element); //root already declared
+}
+
+setInterval(tick, 1000);
+
+
+//////////////////////////////////////
+// Components let you split the UI into independent, 
+// reusable pieces, and think about each piece in isolation
+
+// components are like JavaScript functions. 
+// They accept arbitrary inputs (called “props”) 
+// and return React elements describing what should appear on the screen.
+
+//accepts a single object with data and returns a react element
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+or with class
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+
+**the element has a (html line function) component's name
+**beside it specify the property key and value
+**then root.render(element)
+
+
+//{name: "Sara"} is the prop, 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const element = <Welcome name="Sara" />;
+root.render(element);
+
+//component names start with capital letter
+//DOM tags start with lower case 
+
+
+**
+//an "App" component that renders "Welcome" component 
+//many times with different values
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
+
+
+/*/////////////////////////////////*/
+/*/////////////////////////////////*/
+/*
+//if part of UI is complex or used several times
+//try to extract from it into separate components
+
+
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+//      <div className="UserInfo">
+///        <img className="Avatar"
+///          src={props.author.avatarUrl}
+///          alt={props.author.name}
+///        />
+//        <div className="UserInfo-name">
+//          {props.author.name}
+//        </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+
+//function uses props = component
+//contains divs and image with className
+//the image has attr of {props.}
+//the divs have content of {props.} 
+and another component with passed {props.}
+//props used: author.avatarUrl/name, text, date
+
+
+////took this and wrapped into function(props) and return
+
+function Avatar (props) {
+    return (
+//        <img className="Avatar"
+//          src={props.user.avatarUrl}
+//          alt={props.user.name}
+//        />
+    );
+}
+
+
+//like welcome and i need only user other the nested keys are in the component
+///<Avatar user={props.author} />
+//will be placed instead of the image tag
+
+/*/////////////////////////////////*/
+/*
+
+function UserInfo (props) {
+    return (
+
+//      <div className="UserInfo">
+///        <Avatar user=props.user />
+//        <div className="UserInfo-name">
+//          {props.user.name}
+//        </div>
+
+    );
+}
+
+
+<UserInfo user={props.author} /> //author is passed to comment
+                                 //user is used inside the components
+
+
+
+
+*/
+
+/*/////////////////////////////////*/
+/*/////////////////////////////////*/
+/*
+
+//components must not change their props (parameters)
+//they can return values depending on the inputs
+//but not return the inputs changed
+
+//Rule: All React components must act like pure functions 
+//with respect to their props.
+
+
+
+//Ok
+function sum(a, b) {
+  return a + b;
+}
+
+//not Ok
+function withdraw(account, amount) {
+  account.total -= amount;
+}
+
+
+//state, allows react components to change their output
+//over time in response to user actions, network responses etc.
+//without violating this rule
+
+https://reactjs.org/docs/components-and-props.html
+
+
+
+/*/////////////////////////////////*/
+/*/////////////////////////////////*/
+/*
+
+
+React is a library for creating user interfaces
+builds single components or widgets
+angular 2 supports building whole single page applications
+
+import React from "react";
+import { render } from "react-dom";
+
+//App is the root component
+//can only return one element/wrapper but it can have other nested elements
+//can use bootstrap class names in className to give style
+class App extends React.Component {
+
+    //needs to return what should be rendered
+    render () {
+        return (
+            <div> 
+                <h1> Hello!</h1>
+            </div>
+        )
+    }
+}
+
+//in html
+<div id="app"> </div
+
+render(<App/>, window..Document.getElementByID("app"));
+
+/*/////////////////////////////////*/
+/*/////////////////////////////////*/
+/*
+
+//create new components in the components folder
+//header.js and home.js
+
+//header.js
+
+import React from "react";
+
+export Class Header extends React.Component {
+
+    render () {
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container">
+                    <div className="navbar-header">
+                        <div className="nav navbar-nav">
+                            <li><a href="#"> Home</a></li>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+
+}
+
+
+////in Home.js
+
+import React from "react";
+
+export class Home extends React.Component {
+    render () {
+        return (
+            <div>
+                <p> In a new Component </p>
+            </div>
+        );
+    }
+}
+
+
+////in index.js
+
+import React from "react";
+import { render } from "react-dom";
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
+class App extends React.Component {
+
+    render () {
+        return (
+            <div className="container"> 
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Header/>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Home/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+//in html
+<div id="app"> </div
+
+render(<App/>, window..Document.getElementByID("app"));
+
+
+
+/*////////////////////////////////////////////////////////////////////*/
+/*
+
+
+//react library has nothing to do with the browser
+//so we use react-dom (was included before)
+
+//setting up React environment without create-new-react-app
+
+# npm init
+# npm i react react-dom
+
+//webpack is what allows react to share modules together
+//and webpack dev server for having a local host
+
+# npm i --save-dev webpack webpack-dev-server webpack-cli
+
+
+//transpile ES6 code into browser friendly code
+//babel preset react and babel preset env
+//also a loader to compile jsx
+//html webpack plugin
+
+# npm i --save-dev babel-core babel-loader babel-preset-env babel-preset-react html-webpack-plugin
+
+>>create webpack.config.js
 
 
 
