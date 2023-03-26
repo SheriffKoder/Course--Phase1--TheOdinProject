68d5c2bf6bc7d54afc722e73c655abd39c5530ae
SheriffKoder <sheriff.koder@hotmail.com>
2023-03-26 22:27:29 +0200

    React: lifecycle methods

--- Odin5-1.js
+++ Odin5-1.js
@@ -393,6 +393,9 @@ in function components.
 In the rare cases where it’s important for the code to run before browser paint, useLayoutEffect is a closer match.
 
 
+/*////////////////////////////////////////////////////////////////////*/
+/*
+
 //componentDidUpdate(prevProps, prevState, snapshot?)
 React will call it immediately after your component has been re-rendered with updated props or state. 
 You can use it to manipulate the DOM after an update
@@ -414,6 +417,250 @@ The logic inside componentDidUpdate should usually be wrapped
 in conditions comparing this.props with prevProps, 
 and this.state with prevState. Otherwise, there’s a 
 risk of creating infinite loops.
+
+
+
+/*////////////////////////////////////////////////////////////////////*/
+/*
+
+componentWillMount() > Deprecated
+now named to UNSAFE_componentWillMount
+Run the rename-unsafe-lifecycles codemod to automatically update your components.
+
+
+componentWillReceiveProps(nextProps) > Deprecated
+now named UNSAFE_componentWillReceiveProps
+
+
+componentWillUpdate(nextProps, nextState) > Deprecated
+now named UNSAFE_componentWillUpdate
+
+getChildContext() 
+This API will be removed in a future major version of React. 
+Use Context.Provider instead.
+Lets you specify the values for the legacy context is provided 
+by this component.
+
+
+
+
+
+/*////////////////////////////////////////////////////////////////////*/
+/*
+componentWillUnmount() 
+React will call it before your component is removed (unmounted) from the screen
+This is a common place to cancel data fetching or remove subscriptions.
+
+The logic inside componentWillUnmount should “mirror” the logic inside componentDidMount.
+
+componentWillUnmount does not take any parameters.
+componentWillUnmount should not return anything.
+
+When Strict Mode is on, in development React will call 
+componentDidMount, then immediately call componentWillUnmount
+
+
+
+/*////////////////////////////////////////////////////////////////////*/
+/*
+forceUpdate(callback?) 
+forces a component to re-render
+
+If your component’s render method only reads from this.props, 
+this.state, or this.context, it will re-render automatically 
+when you call setState inside your component or one of its parents.
+
+if your component’s render method reads directly from an external data source,
+you have to tell React to update the user interface when that data source changes. 
+That’s what forceUpdate lets you do.
+
+Try to avoid all uses of forceUpdate and only read from this.props 
+and this.state in render.
+
+optional callback parameter If specified, 
+React will call the callback you’ve provided after the update is committed.
+
+forceUpdate does not return anything.
+
+If you call forceUpdate, React will re-render 
+without calling shouldComponentUpdate.
+
+Reading an external data source and forcing class components 
+to re-render in response to its changes with forceUpdate 
+has been superseded by useSyncExternalStore in function components.
+
+
+/*////////////////////////////////////////////////////////////////////*/
+/*
+getSnapshotBeforeUpdate(prevProps, prevState) 
+React will call it immediately before React updates the DOM
+
+enables your component to capture some information from the DOM 
+(e.g. scroll position) before it is potentially changed.
+
+Any value returned by this lifecycle method will be passed 
+as a parameter to componentDidUpdate.
+
+class ScrollingList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.listRef = React.createRef();
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    // Are we adding new items to the list?
+    // Capture the scroll position so we can adjust scroll later.
+    if (prevProps.list.length < this.props.list.length) {
+      const list = this.listRef.current;
+      return list.scrollHeight - list.scrollTop;
+    }
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    // If we have a snapshot value, we've just added new items.
+    // Adjust scroll so these new items don't push the old ones out of view.
+    // (snapshot here is the value returned from getSnapshotBeforeUpdate)
+    if (snapshot !== null) {
+      const list = this.listRef.current;
+      list.scrollTop = list.scrollHeight - snapshot;
+    }
+  }
+
+  render() {
+    return (
+      <div ref={this.listRef}>{/* ...contents...}</div>
+      );
+    }
+  }
+
+In the above example, it is important to read the scrollHeight 
+property directly in getSnapshotBeforeUpdate. It is not safe to 
+read it in render, UNSAFE_componentWillReceiveProps, 
+or UNSAFE_componentWillUpdate because there is a potential time gap 
+between these methods getting called and React updating the DOM.
+
+Parameters
+prevProps: Props before the update. Compare prevProps to this.props to determine what changed.
+prevState: State before the update. Compare prevState to this.state to determine what changed.
+
+Returns 
+You should return a snapshot value of any type that you’d like, 
+or null. The value you returned will be passed as the third argument 
+to componentDidUpdate.
+
+getSnapshotBeforeUpdate will not get called if shouldComponentUpdate 
+is defined and returns false.
+
+At the moment, there is no equivalent to getSnapshotBeforeUpdate for
+function components. This use case is very uncommon, 
+but if you have the need for it, for now you’ll have to write a class component.
+
+
+/*////////////////////////////////////////////////////////////////////*/
+/*
+render()
+The render method is the only required method in a class component.
+
+specify what you want to appear on the screen, for example
+
+usually the render method should return a piece of JSX
+and other return types are supported like strings etc.
+
+render method written as a pure function
+should not interact with the browser API
+side effects should happen either in event handlers or methods
+like componentDidMount
+
+Parameters;
+prevProps: Props before the update. Compare prevProps to this.props to determine what changed.
+prevState: State before the update. Compare prevState to this.state to determine what changed.
+
+Returns
+render can return any valid React node. 
+This includes React elements such as <div />, strings, 
+numbers, portals, empty nodes (null, undefined, true, and false), 
+and arrays of React nodes.
+
+When Strict Mode is on, React will call render twice in development 
+and then throw away one of the results. This helps you notice 
+the accidental side effects that need to be moved out of the 
+render method.
+
+There is no one-to-one correspondence between the render call 
+and the subsequent componentDidMount or componentDidUpdate call. 
+Some of the render call results may be discarded by React 
+when it’s beneficial.
+
+
+
+/*////////////////////////////////////////////////////////////////////*/
+/*
+setState(nextState, callback?) 
+
+setState enqueues changes to the component state. 
+It tells React that this component and its children need to 
+re-render with the new state. This is the main way you’ll 
+update the user interface in response to interactions.
+
+It only affects what this.state will return starting from the next render.
+
+
+You can also pass a function to setState. It lets you update state based on the previous state:
+
+  handleIncreaseAge = () => {
+    this.setState(prevState => {
+      return {
+        age: prevState.age + 1
+      };
+    });
+  }
+You don’t have to do this, but it’s handy if you want to update 
+state multiple times during the same event.
+
+
+Parameters:
+,,next state, object or a function
+an object will be shallowly merged into this.state
+
+a function, will be treated as an updater function
+must be pure
+should take the pending state and props as arguments
+should return the object to be shallowly merged into this.state
+
+React will put your updater function in a queue and re-render your component
+During the next render, react will calculate the next state
+by applying all of the queued updaters to the previous state
+
+,,optional callback: If specified, React will call the callback 
+you’ve provided after the update is committed.
+
+setState does not return anything.
+
+setState is a request more than a command
+react batches the updates and re-render together in a single pass at
+the end of the event for multiple components
+
+setState does not update this.state immediatley
+this makes reading this.state right after calling setState
+a potential pitfall
+instead use componentDidUpdate or the setState callback argument
+are guaranteed to fire after the update has been applied
+
+if you need to set the state based on the prev state,
+can pass function to nextState as described above
+
+calling setState in class components is simillar to calling
+a set function in function components
+
+/*////////////////////////////////////////////////////////////////////*/
+/*
+shouldComponentUpdate(nextProps, nextState, nextContext) 
+React will call it to determine whether a re-render can be skipped.
+
+If you are confident you want to write it by hand, you may compare 
+this.props with nextProps and this.state with nextState and 
+return false to tell React the update can be skipped.
 
 
 
